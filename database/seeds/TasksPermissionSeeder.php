40c2fcb4a761e3c5eb378e9c28018f42fde2c3bb
Jimmi Robles <55005706+jimmiroblescasanova@users.noreply.github.com>
2021-03-23 16:44:54 -0500

    Update TasksPermissionSeeder.php

--- database/seeds/TasksPermissionSeeder.php
+++ database/seeds/TasksPermissionSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 
@@ -12,7 +13,11 @@ class TasksPermissionSeeder extends Seeder
      */
     public function run()
     {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
         // Configurations tasks
         Permission::create(['name'=> 'run_tasks']);
+        $user = User::find(1);
+        $user->givePermissionTo('run_tasks');
     }
 }
