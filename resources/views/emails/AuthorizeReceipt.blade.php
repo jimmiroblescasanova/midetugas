6d172eaa49e7992aca3bc320d32182d5217e8ccf
Jimmi Robles <55005706+jimmiroblescasanova@users.noreply.github.com>
2022-09-27 14:37:20 -0500

    Update AuthorizeReceipt.blade.php
    Datos del correo al autorizar

--- resources/views/emails/AuthorizeReceipt.blade.php
+++ resources/views/emails/AuthorizeReceipt.blade.php
@@ -1,8 +1,8 @@
 @component('mail::message')
-# Efigas Smart
+# Midetugas
 
-Su recibo de Efigas Smart se encuentra listo y se lo enviamos adjunto en este correo electrónico.
-Para su comodidad puede pagar su recibo por medio de transferencia electrónica o depósito en efectivo en Oxxo.
+Su recibo de consumo de gas, se encuentra listo y se lo enviamos adjunto en este correo electrónico.
+Para su comodidad puede pagarlo por medio de transferencia electrónica, paypal o depósito en efectivo en Oxxo.
 
 Gracias,<br>
 {{ config('app.name') }}
